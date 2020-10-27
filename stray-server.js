547fee51bb36ae83fb6e9b291c5a4efaed9c0571
Wei-Hao Wang <wwang03@risd.edu>
2020-10-27 14:16:09 +0800

    add data for all chapters

--- stray-server.js
+++ stray-server.js
@@ -59,7 +59,10 @@ var slide_image_length = 138;
 var chapter_1_length = 588;
 var chapter_2_length = 40;
 var chapter_3_length = 309;
+var chapter_4_length = 1;
+var chapter_5_length = 1;
 var chapter_6_length = 404;
+var chapter_7_length = 1;
 var chapter_8_length = 100;
 
 
@@ -82,8 +85,40 @@ app.get("/now", (req, res, next) => {
   var current_letter = Math.round( (now/letter_duration ) % letter_length);
   var current_slide_text = parseInt(Math.round( (now/slide_text_duration )) % slide_text_length);
   var current_slide_image = parseInt(Math.round( (now/slide_image_duration )) % slide_image_length);
+  var chapter_1 = {
+    length: chapter_1_length,
+    current_slide: parseInt(Math.round( (now/slide_text_duration )) % chapter_1_length)
+  };
+  var chapter_2 = {
+    length: chapter_2_length,
+    current_slide: parseInt(Math.round( (now/slide_text_duration )) % chapter_2_length)
+  }
+  var chapter_3 = {
+    length: chapter_3_length,
+    current_slide: parseInt(Math.round( (now/slide_text_duration )) % chapter_3_length)
+  }
+  var chapter_4 = {
+    length: chapter_4_length,
+    current_slide: parseInt(Math.round( (now/slide_text_duration )) % chapter_4_length)
+  }
+  var chapter_5 = {
+    length: chapter_5_length,
+    current_slide: parseInt(Math.round( (now/slide_text_duration )) % chapter_5_length)
+  }
+  var chapter_6 = {
+    length: chapter_6_length,
+    current_slide: parseInt(Math.round( (now/slide_text_duration )) % chapter_6_length)
+  }
+  var chapter_7 = {
+    length: chapter_7_length,
+    current_slide: parseInt(Math.round( (now/slide_text_duration )) % chapter_7_length)
+  }
+  var chapter_8 = {
+    length: chapter_8_length,
+    current_slide: parseInt(Math.round( (now/slide_text_duration )) % chapter_8_length)
+  }
 
-  // now = now / 1000;    // seconds since 1970 unix time
+  now = now / 1000;    // seconds since 1970 unix time
   res.json({ 
   	now: now, 
   	poem: poem, 
@@ -96,14 +131,14 @@ app.get("/now", (req, res, next) => {
   	slide_image_length: slide_image_length, 
   	slide_image_duration: slide_image_duration,
   	current_slide_image: current_slide_image,
-    chapter_1: {
-      length: chapter_1_length,
-      current_slide: parseInt(Math.round( (now/slide_text_duration )) % chapter_1_length)
-    },
-    chapter_2: {
-      length: chapter_2_length,
-      current_slide: parseInt(Math.round( (now/slide_text_duration )) % chapter_2_length)
-    }
+    chapter_1: chapter_1,
+    chapter_2: chapter_2,
+    chapter_3: chapter_3,
+    chapter_4: chapter_4,
+    chapter_5: chapter_5,
+    chapter_6: chapter_6,
+    chapter_7: chapter_7,
+    chapter_8: chapter_8
 
   });
 });
